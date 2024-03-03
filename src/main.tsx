599a44d06638a42cd2dd64c0611583ede66f2c52
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-03-03 17:28:08 +0600

    Set up redux, bootstrap, react router and axios

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './app/store';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
